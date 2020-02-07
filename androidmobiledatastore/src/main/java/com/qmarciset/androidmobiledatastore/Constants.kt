3a440c15f94e5a47118ededb2769140211e46e7b
Quentin Marciset <quentin.marciset@4d.com>
2020-02-07 09:43:13 +0100

    adding copyrights

--- androidmobiledatastore/src/main/java/com/qmarciset/androidmobiledatastore/Constants.kt
+++ androidmobiledatastore/src/main/java/com/qmarciset/androidmobiledatastore/Constants.kt
@@ -1,3 +1,9 @@
+/*
+ * Created by Quentin Marciset on 7/2/2020.
+ * 4D SAS
+ * Copyright (c) 2020 Quentin Marciset. All rights reserved.
+ */
+
 package com.qmarciset.androidmobiledatastore
 
 const val DATABASE_NAME = "4d_sample_app_database"
